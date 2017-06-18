3726e2f4f6ea406d9a7cb45e10c22b275a29702b
jasonh88 <jasonh88@gmail.com>
2017-06-18 14:08:56 -0700

    Progress API controller added and tested.  All endpoints functional.

--- Blog/Controllers/Api/ProgressApiController.cs
+++ Blog/Controllers/Api/ProgressApiController.cs
@@ -1,13 +1,51 @@
-﻿using System;
+﻿using Blog.Models.ViewModels;
+using Blog.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace Blog.Controllers.Api
+namespace Blog.Controllers
 {
+    [RoutePrefix("api/progress")]
     public class ProgressApiController : ApiController
     {
+        [HttpGet]
+        [Route]
+        public HttpResponseMessage GetProgress()
+        {
+            ProgressService progSvc = new ProgressService();
+            List<Progress> progressList = progSvc.GetProgress();
+            return Request.CreateResponse(HttpStatusCode.OK, progressList);
+        }
+
+        [HttpPost]
+        [Route]
+        public HttpResponseMessage CreateProgress(Progress model)
+        {
+            ProgressService progSvc = new ProgressService();
+            int id = progSvc.CreateProgress(model);
+            return Request.CreateResponse(HttpStatusCode.OK, id);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public HttpResponseMessage DeleteProgress(int id)
+        {
+            ProgressService progSvc = new ProgressService();
+            progSvc.DeleteProgress(id);
+            return Request.CreateResponse(HttpStatusCode.OK, id);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public HttpResponseMessage UpdateProgress([FromUri] int id, [FromBody]Progress model)
+        {
+            ProgressService progSvc = new ProgressService();
+            progSvc.UpdateProgress(id, model);
+            return Request.CreateResponse(HttpStatusCode.OK, id);
+        }
     }
 }
